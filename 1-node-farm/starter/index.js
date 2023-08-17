0d64d62025bae5d8aeafe80ce8d3a60803fb3795
JuanJoseVillamizarCampus1 <juanjvs1409@gmail.com>
2023-08-17 13:23:20 -0500

    1.0

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -32,8 +32,12 @@ const http = require('http')
 ////////////////////////////////////////
 //SERVER
 const server =  http.createServer((req, res)=>{
-    console.log(req);
-    res.end('Hello from the server!');
+    const pathName = req.url;
+    if (pathName === '/' ||pathName === '/overview') {
+        res.end('This is the OVERVIEW')
+    }
+    else if (pathName === '/product')
+    res.end('This is the PRODUCT')
 });
 
 server.listen(8000,'127.0.0.1',()=>{
